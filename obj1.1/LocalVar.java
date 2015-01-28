0e68ae2fd662f35db9ccbae68803b6f5f075a15d
siarhei <s.siarhei@gmail.com>
2015-01-28 21:31:23 +0300

    Objective 1.1: Define the scope of variables

---
+++ obj1.1/LocalVar.java
@@ -0,0 +1,28 @@
+public class LocalVar {
+	//legal array definitions
+	char[] chars2D[];
+	byte[][] bytes2D;
+	short[] numbers;
+	int nums[];
+
+	//illegal
+	//long[10] ls;
+
+	public static int main(String ... s) {
+		int i; //define local var
+
+		if (s != null) {
+			i = 5;
+			System.out.println("Local var is" + i);
+		}
+		return 0;
+	}
+
+	class Inner {
+		//static void doRec() {} //illegal
+	}
+
+	static class Nested {
+		static void doRec() {}
+	}
+}
