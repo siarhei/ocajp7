e14e2650e2a33753613bf9b4b4984ebf07d46d83
siarhei <s.siarhei@gmail.com>
2015-02-10 13:52:51 +0300

    Primitives. Assigning and Casting

---
+++ obj2.1/Primitive.java
@@ -0,0 +1,50 @@
+class PrimitiveAssignment {
+	byte a = 3;
+	byte b = 5;
+	//byte c = a + b; //error: possible loss of precision
+	byte c = (byte)(a + b);
+
+	//ok for instance var-s
+	int i, j, k=i+5;
+	void doCall() {
+		//int i, j, k=i+5; //error: variable i might not have been initialized
+	}
+}
+
+class PrimitiveCasting {
+	int a = 100;
+	long b = a; //implicit
+
+	float a1 = 10.001F;
+	int b1 = (int) a1; //explicit
+	//int x = 10.49; //error: possible loss of precision {double to int}
+
+	double d = 10L; //OK
+
+	long ln = 56L;
+	byte bt = (byte) ln;
+
+	long l2 = 130; //implicit {int to long}
+	byte b2 = (byte) l2; //-126
+
+	float f = 234.56F;
+	short s = (short)f;
+
+	//float f2 = 32.3; //error: possible loss of precision {double to float}
+	float f3 = (float)32.3;
+
+	//byte b3 = 128; //127 is max positive
+	byte b4 = 127;
+	byte b5 = (byte) 128;
+	/*
+		128 - 10000000
+		inverse - 01111111
+		result is -(inverse + 1) => -10000000 => -128 => b5 == -128
+	*/
+
+	byte b6 = 10;
+	{ b6 += 25; } //no casting is required!!!!!!!!
+	//BUT
+	byte b7 = 5;
+	//{ b7 = b7 + 9; } //error: possible loss of precision
+}
